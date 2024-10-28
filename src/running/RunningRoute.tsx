8ab05ce0a0e7ac8ba4470a05f34f2662b0d27ee2
Abzal Orazbek <abzalorazbek2003@gmail.com>
2024-10-28 15:40:08 +0500

    add totalCalories and distance

--- src/running/RunningRoute.tsx
+++ src/running/RunningRoute.tsx
@@ -6,25 +6,69 @@ import {
 import React from 'react';
 import {ToastAndroid, View} from 'react-native';
 import {Button, Text} from 'react-native-paper';
+import {UpdateEndLocation, UpdateStartLocation} from '../map/MapPins';
 
 function RunningRoute() {
   const [isSessionActive, setIsSessionActive] = React.useState(false);
   const [startTime, setStartTime] = React.useState<Date | null>(null);
   const [endTime, setEndTime] = React.useState<Date | null>(null);
   const [steps, setSteps] = React.useState<number | null>(null);
+  const [totalCalories, setTotalCalories] = React.useState<number | null>(null);
+  const [distance, setDistance] = React.useState<number | null>(null);
 
   async function readSampleData(start: Date) {
     await initialize();
-    await requestPermission([{accessType: 'read', recordType: 'Steps'}]);
+    await requestPermission([
+      {accessType: 'read', recordType: 'Steps'},
+      {accessType: 'read', recordType: 'TotalCaloriesBurned'},
+      {accessType: 'read', recordType: 'Distance'},
+    ]);
+
     readRecords('Steps', {
       timeRangeFilter: {
         operator: 'after',
         startTime: start.toISOString(),
       },
     }).then(({records}) => {
-      let stepResult = records.reduce((acc, record) => acc + record.count, 0);
-      setSteps(stepResult);
-      ToastAndroid.show(`Total steps: ${stepResult}`, ToastAndroid.SHORT);
+      let result = records.reduce((acc, record) => acc + record.count, 0);
+      setSteps(result);
+    });
+
+    readRecords('TotalCaloriesBurned', {
+      timeRangeFilter: {
+        operator: 'after',
+        startTime: start.toISOString(),
+      },
+    }).then(({records}) => {
+      let result = records.reduce(
+        (acc, record) => acc + record.energy.inCalories,
+        0,
+      );
+      setTotalCalories(result);
+    });
+
+    readRecords('Distance', {
+      timeRangeFilter: {
+        operator: 'after',
+        startTime: start.toISOString(),
+      },
+    }).then(({records}) => {
+      let result = records.reduce(
+        (acc, record) => acc + record.distance.inMeters,
+        0,
+      );
+      setDistance(result);
+    });
+
+    readRecords('Speed', {
+      timeRangeFilter: {
+        operator: 'after',
+        startTime: start.toISOString(),
+      },
+    }).then(({records}) => {
+      // average speed
+      //
+      // setTotalCalories(result);
     });
   }
 
@@ -32,11 +76,13 @@ function RunningRoute() {
     setIsSessionActive(true);
     setStartTime(new Date());
     setSteps(null);
+    UpdateStartLocation();
   }
 
   function stopSession() {
     setIsSessionActive(false);
     setEndTime(new Date());
+    UpdateEndLocation();
   }
 
   function testSteps() {
@@ -83,6 +129,12 @@ function RunningRoute() {
       {startTime !== null && <Text>Start Time: {startTime.toString()}</Text>}
       {endTime !== null && <Text>End Time: {endTime.toString()}</Text>}
       {steps !== null && <Text key="stepCounter">Steps: {steps}</Text>}
+      {totalCalories !== null && (
+        <Text key="caloriesCounter">Calories: {totalCalories}</Text>
+      )}
+      {distance !== null && (
+        <Text key="distanceCounter">Distance: {distance}</Text>
+      )}
     </View>
   );
 }
